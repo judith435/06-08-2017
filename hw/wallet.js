204557f05b3f6d5320d73934226180dc6b6f5f6c
יהודית אילסון <JudithI@kkl.org.il>
2017-08-08 16:50:05 +0300

    save

--- hw/wallet.js
+++ hw/wallet.js
@@ -1,14 +1,15 @@
 'use strict'
 
 var purchase = {
+    amount_purchased: 0,
     amount_available: 0,
     setAmountAvailable: function(amount){
         if (!isNaN(parseFloat(amount)) ){
             this.amount_available = amount;
         }
         else{
-            this.amount_available = 4300;
-            alert("you did not enter a valid amount - you have thus received the default amount of 4,300 nis ");
+                this.amount_available = 4300;
+                alert("you did not enter a valid amount - you have thus received the default amount of 4,300 nis ");
         }
     }, 
     vat: 0.17,
@@ -34,17 +35,19 @@ var purchase = {
         while(this.amount_available >= cell_price_with_vat){
             this.cellphones.push(new this.cellphone(cell_price_with_vat));
             this.amount_available -= cell_price_with_vat;
+            this.amount_purchased += cell_price_with_vat;
         }
-        console.log("amount left: " + this.amount_available + "  number of phones: " + this.cellphones.length);
 
         let accessory_price_with_vat = this.calc_price_with_vat(this.accessory_price_before_vat)
         while(this.amount_available >= accessory_price_with_vat){
             this.accessories.push(new this.accessory(accessory_price_with_vat));
             this.amount_available -= accessory_price_with_vat;
+            this.amount_purchased += accessory_price_with_vat;
         }
 
-        console.log("amount left: " + this.amount_available + "  number of accessories: " + this.accessories.length);
-
+        console.log("You can purchase " + this.cellphones.length + " smartphones and " + this.accessories.length 
+                        + " smartphone accessories at a  total cost of " + this.amount_purchased.toFixed(2)  + " nis - leaving you "
+                        +  this.amount_available.toFixed(2) + " nis change");
     },
 }
 
